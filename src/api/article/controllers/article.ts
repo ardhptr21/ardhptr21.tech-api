3f74f968facb4d91dd60850fd236c9788785faa2
ardhptr21 <ardhiputrapradana21@gmail.com>
2023-03-25 23:16:47 +0700

    added CKEditor and modify Article collection to find by slug

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -2,6 +2,20 @@
  * article controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::article.article');
+export default factories.createCoreController(
+  "api::article.article",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const entity = await strapi.db.query("api::article.article").findOne({
+        where: { slug: id },
+      });
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity, ctx);
+    },
+  })
+);
